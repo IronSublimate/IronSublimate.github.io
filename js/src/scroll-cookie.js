812402a5049149a2c90ac8d1a160a8dc0580eb20
IronSublimate <houyuxuan3120487@163.com>
2020-10-06 17:16:14 +0800

    Site updated: 2020-10-06 17:16:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 06 2020 17:08:39 GMT+0800 (中国标准时间)
+// build time:Tue Oct 06 2020 17:15:49 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
